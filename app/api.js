fe734c42e29b7a27ec0643bb22b27864161719ab
Alaa Jerbi <mralaajerbi@gmail.com>
2021-01-16 11:03:41 +0100

    Add API

---
+++ app/api.js
@@ -0,0 +1,14 @@
+const fibonacci = (n) => {
+    if (n <= 0) return 0
+    else if (n == 1) return 1
+    else return fibonacci(n-1) + fibonacci(n-2);
+}  
+
+const factorial = (n) => {
+    return n <= 1 ? 1 : factorial(n - 1) * n; 
+}
+
+module.exports = {
+    fibonacci,
+    factorial
+}
